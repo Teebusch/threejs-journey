2d49ec30121780bb847cee4d82da3baffed8e2db
Tobias Busch <teebusch@gmail.com>
2021-09-18 22:34:02 +0200

    finish lesson6

--- src/script.js
+++ src/script.js
@@ -1,5 +1,6 @@
 import './style.css'
 import * as THREE from 'three'
+import gsap from 'gsap'
 
 const scene = new THREE.Scene()
 
@@ -26,4 +27,20 @@ const renderer = new THREE.WebGLRenderer({
 })
 
 renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+
+const clock = new THREE.Clock()
+
+gsap.to(mesh.position, { duration: 1, delay: 0.1, x: 2 })
+
+function tick( ) {
+
+    // const elapsedTime = clock.getElapsedTime()
+    // mesh.position.x = Math.sin(elapsedTime)
+    // mesh.position.y = Math.cos(elapsedTime)
+    // camera.lookAt(mesh.position)
+
+    renderer.render(scene, camera)
+    window.requestAnimationFrame(tick)
+}
+
+tick()
