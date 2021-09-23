132885c9f8c6e8a90c41cd9122a7d8b2670a0585
Tobias Busch <teebusch@gmail.com>
2021-09-24 00:09:14 +0200

    wwbkit trouble

--- src/script.js
+++ src/script.js
@@ -2,7 +2,9 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
+
 const scene = new THREE.Scene()
+
 
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0xE3AC5A })
@@ -11,11 +13,11 @@ scene.add(mesh)
 
 
 const sizes = {
-    width: 800,
-    height: 600
+    width: window.innerWidth,
+    height: window.innerHeight
 }
 
-const aspectRatio = sizes.width / sizes.height
+let aspectRatio = sizes.width / sizes.height
 
 const camera = new THREE.PerspectiveCamera(45, aspectRatio, 1, 100)
 camera.position.y = 5
@@ -28,11 +30,43 @@ const renderer = new THREE.WebGLRenderer({
 })
 
 renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 const Clock = new THREE.Clock()
 
 const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
+
+
+const fullscreenElement = document.fullscreenElement || document.webkitFullscreenElement
+
+window.addEventListener('dblclick', (event) => {
+    if (!fullscreenElement) {
+        if (canvas.requestFullscreen) {
+            canvas.requestFullscreen()
+        } else if (canvas.webkitRsequestFullscreen) {
+            canvas.webkitRequestFullscreen()
+        }
+    } else {
+        if (document.exitFullscreen) {
+            document.exitFullscreen()
+        } else if (document.webkitExitFullscreen) {
+            document.webkitExitFullscreen()
+        }
+    }
+})
+
+window.addEventListener('resize', (event) => {
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+    aspectRatio = sizes.width / sizes.height
+    camera.aspect = aspectRatio
+    
+    camera.updateProjectionMatrix()
+    renderer.setSize(sizes.width, sizes.height)
+
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
 
 function tick() {
     // update scene
