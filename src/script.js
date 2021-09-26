c9b054fd201da676f56c0be1dbb2311da228c514
Tobias Busch <teebusch@gmail.com>
2021-09-26 10:41:48 +0200

    ad resize to boilerplate, some comments

--- src/script.js
+++ src/script.js
@@ -3,15 +3,7 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 
-const scene = new THREE.Scene()
-
-
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xE3AC5A })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
-
-
+// canvas size
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
@@ -19,71 +11,55 @@ const sizes = {
 
 let aspectRatio = sizes.width / sizes.height
 
+
+// scene
+const scene = new THREE.Scene()
+
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xE3AC5A })
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+
+// camera
 const camera = new THREE.PerspectiveCamera(45, aspectRatio, 1, 100)
 camera.position.y = 5
 scene.add(camera)
 
-const canvas = document.querySelector(".webgl")
-const renderer = new THREE.WebGLRenderer({
-    canvas,
-    alpha: true
-})
 
+// renderer
+const canvas = document.querySelector(".webgl")
+const renderer = new THREE.WebGLRenderer({ canvas, alpha: true })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
-const Clock = new THREE.Clock()
 
+// interactive controls
 const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
 
 
-
-window.addEventListener('dblclick', (event) => {
-
-    const fullscreenElement = document.fullscreenElement || document.webkitFullscreenElement
-
-    if (!fullscreenElement) 
-    {
-        if (canvas.requestFullscreen) 
-        {
-            canvas.requestFullscreen()
-        } 
-        else if (canvas.webkitRequestFullscreen) 
-        {
-            canvas.webkitRequestFullscreen()
-        }
-    } 
-    else 
-    {
-        if (document.exitFullscreen) 
-        {
-            document.exitFullscreen()
-        } 
-        else if (document.webkitExitFullscreen) 
-        {
-            document.webkitExitFullscreen()
-        }
-    }
-})
-
-window.addEventListener('resize', (event) => {
+// resize render when window size changes
+window.addEventListener('resize', () => {
     sizes.width = window.innerWidth
     sizes.height = window.innerHeight
     aspectRatio = sizes.width / sizes.height
+
     camera.aspect = aspectRatio
     
     camera.updateProjectionMatrix()
     renderer.setSize(sizes.width, sizes.height)
-
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
+
+// animation loop
+const Clock = new THREE.Clock()
+
 function tick() {
-    // update scene
     mesh.rotation.y = Clock.getElapsedTime()
 
-    controls.update() // needed if using damping with controls
+    controls.update() // required when using damping with controls
 
     renderer.render(scene, camera)
     window.requestAnimationFrame(tick)
