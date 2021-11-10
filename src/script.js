3af91219940953702b0f0265bf9e8c9ac927cee5
Tobias Busch <teebusch@gmail.com>
2021-11-10 20:34:52 +0100

    finish lesson

--- src/script.js
+++ src/script.js
@@ -1,8 +1,11 @@
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js'
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js'
 import * as lilgui from 'lil-gui'
 import gsap from 'gsap'
+
 
 // canvas size
 const sizes = {
@@ -12,31 +15,108 @@ const sizes = {
 
 let aspectRatio = sizes.width / sizes.height
 
+
 const parameters = {
-    color: 0xE3AC5A,
+    color: 0x660022,
     spin: () => {
         gsap.to(mesh.rotation, { duration: 2, y:  mesh.rotation.y + 4 * Math.PI })
     }
 }
 
+
+
 // scene
 const scene = new THREE.Scene()
+scene.fog = new THREE.Fog(parameters.color, 5, 7);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: parameters.color })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+// const axesHelper = new THREE.AxesHelper()
+// scene.add(axesHelper)
+
+// textures
+
+const textureLoader = new THREE.TextureLoader()
+const textTexture = textureLoader.load('matcap1.png')
+const donutTexture = textureLoader.load('matcap2.png')
+
+// fonts
+
+const textMaterial = new THREE.MeshMatcapMaterial({
+    matcap: textTexture,
+})
+
+const fontLoader = new FontLoader()
+//fontLoader.load('fonts/helvetiker_regular.typeface.json', (font) => {
+fontLoader.load('fonts/Bebas Neue_Regular.json', (font) => {
+
+    const textGeometry = new TextGeometry('Hello\nWorld!', 
+    {
+        font,
+        size: 0.6,
+        height: 0.1,
+        curveSegments: 6,
+        bevelEnabled: true,
+        bevelThickness: 0.01,
+        bevelSize: 0.01,
+        bevelSegments: 2,
+        bevelOffset: 0
+    })
+    
+    textGeometry.center()
+    
+    const text = new THREE.Mesh(textGeometry, textMaterial)
+    text.rotateY(-0.1)
+    scene.add(text)
+})
+
+
+
+// donuts
+
+const donutGeometry = new THREE.TorusGeometry(0.1, 0.05, 20, 25)
+const donutMaterial = new THREE.MeshMatcapMaterial({
+    matcap: donutTexture,
+    transparent: true,
+    opacity: 0.5
+})
+
+donutMaterial.blending = THREE.AdditiveBlending
+
+console.time('donuts')
+
+const donuts = new THREE.Group()
+
+for(let i = 0; i < 300; i++)
+{
+    const donut = new THREE.Mesh(donutGeometry, donutMaterial)
+
+    donut.position.x = (Math.random()-0.5) * 5
+    donut.position.y = (Math.random()-0.5) * 5
+    donut.position.z = (Math.random()-0.5) * 5
+
+    donut.rotation.x = Math.random() * Math.PI
+    donut.rotation.y = Math.random() * Math.PI
+
+    const scale = Math.max(0.2, Math.random())
+    donut.scale.set(scale, scale, scale)
+    donuts.add(donut)
+}
+
+scene.add(donuts) 
+
+console.timeEnd('donuts')
 
 
 // camera
+
 const camera = new THREE.PerspectiveCamera(45, aspectRatio, 1, 100)
-camera.position.y = 5
+camera.position.z = 5
 scene.add(camera)
 
 
 // renderer
+
 const canvas = document.querySelector(".webgl")
-const renderer = new THREE.WebGLRenderer({ canvas, alpha: true })
+const renderer = new THREE.WebGLRenderer({ canvas, alpha: true, antialias: true })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
@@ -63,11 +143,9 @@ controls.enableDamping = true
 // debug - press h to hide / unhide, or use gui.hide()
 const gui = new lilgui.GUI({ width: 300 })
 
-gui.add(mesh.position, 'y', -3, 3, 0.1).name("elevation")
-gui.add(material, 'wireframe')
 gui.add(parameters, 'spin')
 gui.addColor(parameters, 'color').onChange(() => {
-    material.color.set(parameters.color)
+    scene.fog.color.set(parameters.color)
 })
 
 
@@ -76,12 +154,13 @@ const Clock = new THREE.Clock()
 
 function tick() {
      const elapsedTime = Clock.getElapsedTime()
-     // mesh.rotation.y = 2* Math.PI * (Math.cos(elapsedTime) + 0.5)
+     donuts.rotation.y = 0.01 * Math.PI * (elapsedTime)
 
     controls.update() // required when using damping with controls
 
     renderer.render(scene, camera)
     window.requestAnimationFrame(tick)
 }
+
 
 tick()
