2d9e6af35b61339c6313604492a42374da398430
Tobias Busch <teebusch@gmail.com>
2021-11-16 21:46:22 +0100

    shadow on directional light

--- src/script.js
+++ src/script.js
@@ -38,22 +38,60 @@ let aspectRatio = sizes.width / sizes.height
 const scene = new THREE.Scene()
 
 
+// Ambient light
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
+scene.add(ambientLight)
 
-// starter cube
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ 
-    color: parameters.color 
-})
-const mesh = new THREE.Mesh(geometry, material)
-mesh.rotateY(2)
-scene.add(mesh)
+// Directional light
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5)
+directionalLight.position.set(2, 2, - 1)
+directionalLight.castShadow = true
+directionalLight.shadow.mapSize.width = 1024
+directionalLight.shadow.mapSize.height = 1024
+directionalLight.shadow.camera.top = 2
+directionalLight.shadow.camera.right = 2
+directionalLight.shadow.camera.bottom = -2
+directionalLight.shadow.camera.left = -2
+directionalLight.shadow.camera.near = 1
+directionalLight.shadow.camera.far = 5
+
+// directionalLight.shadow.radius = 10 // shadow blur
+
+scene.add(directionalLight)
+
+const dLightCameraHelper = new THREE.CameraHelper(directionalLight.shadow.camera)
+scene.add(dLightCameraHelper)
+
+
+// material
+const material = new THREE.MeshStandardMaterial()
+material.roughness = 0.7
+
+// objects
+const sphere = new THREE.Mesh(
+    new THREE.SphereGeometry(0.5, 32, 32),
+    material
+)
+
+sphere.castShadow = true
+
+const plane = new THREE.Mesh(
+    new THREE.PlaneGeometry(5, 5),
+    material
+)
+plane.rotation.x = - Math.PI * 0.5
+plane.position.y = - 0.5
+
+plane.receiveShadow = true
+
+scene.add(sphere, plane)
 
 
 
 // camera
 
 const camera = new THREE.PerspectiveCamera(45, aspectRatio, 1, 100)
-camera.position.set(0, 2, 5)
+camera.position.set(1, 4, 4)
 scene.add(camera)
 
 
@@ -64,11 +102,14 @@ const canvas = document.querySelector(".webgl")
 
 const renderer = new THREE.WebGLRenderer({ 
     canvas, 
-    alpha: true 
+    alpha: true
 })
 
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+renderer.shadowMap.enabled = true
+renderer.shadowMap.type = THREE.PCFSoftShadowMap
 
 
 // resize render when window size changes
@@ -97,9 +138,18 @@ controls.enableDamping = true
 
 const gui = new lilgui.GUI({ width: 300 })
 
-gui.add(mesh.position, 'y', -3, 3, 0.1).name("elevation")
+gui.add(sphere.position, 'y', -3, 3, 0.1).name("elevation")
 gui.add(material, 'wireframe')
 gui.add(parameters, 'spin')
+
+gui.add(directionalLight, 'intensity').min(0).max(1).step(0.001)
+gui.add(ambientLight, 'intensity').min(0).max(1).step(0.001)
+gui.add(directionalLight.position, 'x').min(- 5).max(5).step(0.001)
+gui.add(directionalLight.position, 'y').min(- 5).max(5).step(0.001)
+gui.add(directionalLight.position, 'z').min(- 5).max(5).step(0.001)
+gui.add(material, 'metalness').min(0).max(1).step(0.001)
+gui.add(material, 'roughness').min(0).max(1).step(0.001)
+
 gui.addColor(parameters, 'color').onChange(() => {
     material.color.set(parameters.color)
 })
